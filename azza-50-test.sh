8ddd2d54ecce13621dccab39db122e4ded6ac98c
Andrew Worsley <amworsley@gmail.com>
2014-05-01 20:23:34 +1000

    Added some missing package files tests - not passing yet

--- azza-50-test.sh
+++ azza-50-test.sh
@@ -2,9 +2,33 @@
 
 # Simple fast release update testing
 
+check_norefresh() {
+    EXP_OUTPUT=${1:-"azza-updates-50 is up to date"}
+    ARG1=${2:-"-norefresh"}
+    if ./RepositoryMirror.py -c azza-50.cfg $ARG1 \
+      | grep "$EXP_OUTPUT"; then
+        echo "$EXP_OUTPUT - ok"
+    else
+        echo "check_norefresh() expected $EXP_OUTPUT - fail"
+        exit 2
+    fi
+}
+check_uptodate() {
+    if ./RepositoryMirror.py -c azza-50.cfg -norefresh \
+      | grep 'azza-updates-50 is up to date'; then
+        echo "Release up to date - ok"
+    else
+        echo "Release not up to date - fail"
+        exit 2
+    fi
+}
+
 rm azza-updates-50/dists/wheezy/updates/Release
-./RepositoryMirror.py -c azza-50.cfg -norefresh \
-    | grep 'Warning: wheezy/updates - Release file missing'
+
+check_norefresh 'Warning: wheezy/updates - Release file missing' 
+#./RepositoryMirror.py -c azza-50.cfg -norefresh \
+    #| grep 'Warning: wheezy/updates - Release file missing'
+#check_norefresh 'file:azza-updates: 1 release changed' '' 
 ./RepositoryMirror.py -c azza-50.cfg
 if [ -e azza-updates-50/dists/wheezy/updates/Release ]; then 
     echo "Updated Release file - ok"
@@ -12,12 +36,17 @@ else
     echo "Release file - not updated"
     exit 1
 fi
+check_uptodate
 
-if ./RepositoryMirror.py -c azza-50.cfg -norefresh \
-  | grep 'azza-updates-50 is up to date'; then
-    echo "Release up to date - ok"
+echo 
+echo "Testing Missing Package file"
+rm azza-updates-50/dists/wheezy/updates/contrib/binary-amd64/Packages.bz2
+
+check_norefresh 'Warning: wheezy/updates - package file missing' 
+if [ -e azza-updates-50/dists/wheezy/updates/contrib/binary-amd64/Packages.bz2 ]; then 
+    echo "Package file was refreshed - -norefresh option not working..."
 else
-    echo "Release not up to date - fail"
-    exit 2
+    echo "Package file is not updated : -norefresh option working..."
+    exit 1
 fi
-    
+
